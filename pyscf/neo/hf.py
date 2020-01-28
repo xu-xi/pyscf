48a95d5390dcec9f0a9adf71d86ee5110f581375
xuxi0910796 <xuxi0910796@gmail.com>
2020-01-28 16:46:09 -0600

    full quantum (beta)

--- pyscf/neo/hf.py
+++ pyscf/neo/hf.py
@@ -20,7 +20,6 @@ class HF(SCF):
     >>> from pyscf import neo
     >>> mol = neo.Mole()
     >>> mol.build(atom = 'H 0 0 0; F 0 0 0.917', basis = 'ccpvdz')
-    >>> mol.set_quantum_nuclei([0])
     >>> mf = neo.HF(mol)
     >>> mf.scf()
     
@@ -31,79 +30,67 @@ class HF(SCF):
 
         self.mol = mol
         self.dm_elec = None
-        self.dm_nuc = None
+        self.dm_nuc = [None]*self.mol.nuc_num
+        self.verbose = 5
 
-        self.mf_elec = scf.RHF(self.mol.elec)
-        self.mf_elec.init_guess = 'atom'
-        self.mf_elec.get_hcore = self.get_hcore_elec
+    def get_hcore_nuc(self, nole):
+        'get core Hamiltonian for quantum nucleus. Nole is the Mole object for it.'
 
-        self.mf_nuc = scf.RHF(self.mol.nuc) #beta: for single proton
-        self.mf_nuc.get_init_guess = self.get_init_guess_nuc
-        self.mf_nuc.get_hcore = self.get_hcore_nuc
-        self.mf_nuc.get_veff = self.get_veff_nuc_bare
-        self.mf_nuc.get_occ = self.get_occ_nuc
+        i = nole.atom_index
+        mass = 1836.15267343 * self.mol.atom_mass_list()[i] # the mass of quantum nucleus in a.u.
 
-        self.verbose = 4
+        h = nole.intor_symmetric('int1e_kin')/mass
+        h -= nole.intor_symmetric('int1e_nuc')*self.mol._atm[i,0] # times nuclear charge
 
-    def get_hcore_nuc(self, mol=None):
-        'get core Hamiltonian for quantum nuclei'
-        #Z = mol._atm[:,0] # nuclear charge
-        #M = gto.mole.atom_mass_list(mol)*1836 # Note: proton mass
-        if mol == None:
-            mol = self.mol.nuc
+        # Coulomb interactions between quantum nucleus and electrons
+        if isinstance(self.dm_elec, numpy.ndarray):
+            h -= scf.jk.get_jk((nole, nole, self.mol.elec, self.mol.elec), self.dm_elec, scripts='ijkl,lk->ij', aosym ='s4') * self.mol._atm[i,0]
 
-        mass_proton = 1836.15267343
-        h = mol.intor_symmetric('int1e_kin')/mass_proton
-        h -= mol.intor_symmetric('int1e_nuc')
-        if self.dm_elec is not None:
-            h -= scf.jk.get_jk((mol, mol, self.mol.elec, self.mol.elec), self.dm_elec, scripts='ijkl,lk->ij', aosym ='s4')
+        # Coulomb interactions between quantum nuclei
+        for j in range(len(self.dm_nuc)):
+            k = self.mol.nuc[j].atom_index
+            if k != i and isinstance(self.dm_nuc[j], numpy.ndarray):
+                h += scf.jk.get_jk((nole, nole, self.mol.nuc[j], self.mol.nuc[j]), self.dm_nuc[j], scripts='ijkl,lk->ij') * self.mol._atm[i, 0] * self.mol._atm[k, 0] # times nuclear charge
+
         return h
 
     def get_occ_nuc(self, nuc_energy=None, nuc_coeff=None):
-        'label the occupation for protons (high-spin)'
+        'label the occupation for quantum nucleus'
 
         e_idx = numpy.argsort(nuc_energy)
         e_sort = nuc_energy[e_idx]
         nuc_occ = numpy.zeros(nuc_energy.size)
-        nocc = self.mol.nuc_num
+        #nocc = self.mol.nuc_num
+        nocc = 1
         nuc_occ[e_idx[:nocc]] = 1
 
         return nuc_occ
 
-    def get_init_guess_nuc(self, mol=None, key=None):
+    def get_init_guess_nuc(self, nole, key=None):
         '''Generate initial guess density matrix for quantum nuclei from core hamiltonian
 
            Returns:
             Density matrix, 2D ndarray
         '''
-        if mol == None:
-            mol = self.mol.nuc
-        h1n = self.get_hcore_nuc(mol)
-        s1n = scf.hf.get_ovlp(mol)
+        h1n = self.get_hcore_nuc(nole)
+        s1n = nole.intor_symmetric('int1e_ovlp')
         nuc_energy, nuc_coeff = scf.hf.eig(h1n, s1n)
         nuc_occ = self.get_occ_nuc(nuc_energy, nuc_coeff)
 
         return scf.hf.make_rdm1(nuc_coeff, nuc_occ)
     
-    def get_hcore_elec(self, mol=None):
-        'get the matrix of core Hamiltonian of electrons of NEO'
-        if mol == None:
-            mol = self.mol.elec
+    def get_hcore_elec(self, eole=None):
+        'Get the core Hamiltonian for electrons in NEO'
+        if eole == None:
+            eole = self.mol.elec # the Mole object for electrons in NEO
 
-        if self.dm_nuc is not None:
-            jcross = scf.jk.get_jk((mol, mol, self.mol.nuc, self.mol.nuc), self.dm_nuc, scripts='ijkl,lk->ij', aosym ='s4')
-        else:
-            jcross = 0
+        j = 0
+        # Coulomb interactions between electrons and all quantum nuclei
+        for i in range(len(self.dm_nuc)):
+            if isinstance(self.dm_nuc[i], numpy.ndarray):
+                j -= scf.jk.get_jk((eole, eole, self.mol.nuc[i], self.mol.nuc[i]), self.dm_nuc[i], scripts='ijkl,lk->ij', aosym='s4') * self.mol._atm[self.mol.nuc[i].atom_index, 0]
 
-        return scf.hf.get_hcore(mol) - jcross
-
-    def get_veff_elec(self, dm_elec, dm_nuc):
-        'get the HF effective potential for electrons in NEO for given density matrixes of electrons and quantum nuclei'
-        mol = self.mol
-        vj, vk = scf.jk.get_jk(mol.elec, (dm_elec,dm_elec), ('ijkl,ji->kl','ijkl,jk->il'), aosym='s8')
-        jcross = scf.jk.get_jk((mol.elec, mol.elec, mol.nuc, mol.nuc), dm_nuc, scripts='ijkl,lk->ij', aosym ='s4')
-
-        return vj - vk * .5 - jcross
+        return scf.hf.get_hcore(eole) + j
 
     def get_veff_nuc_bare(self, mol, dm, dm_last=None, vhf_last=None, hermi=1, vhfopt=None):
         'NOTE: Only for single quantum proton system.'
@@ -121,41 +108,77 @@ class HF(SCF):
             return vj - vk  + numpy.asarray(vhf_last)
 
     def elec_nuc_coulomb(self, dm_elec, dm_nuc):
-        'get the Coulomb matrix between electrons and quantum nuclei'
+        'the energy of Coulomb interactions between electrons and quantum nuclei'
         mol = self.mol
-        jcross = scf.jk.get_jk((mol.elec, mol.elec, mol.nuc, mol.nuc), dm_nuc, scripts='ijkl,lk->ij', aosym = 's4')
-        return jcross
-        #return numpy.einsum('ij,ij', jcross, dm_elec)
+        jcross = 0
+        for i in range(len(dm_nuc)):
+            jcross -= scf.jk.get_jk((mol.elec, mol.elec, mol.nuc[i], mol.nuc[i]), dm_nuc[i], scripts='ijkl,lk->ij', aosym = 's4') * mol._atm[mol.nuc[i].atom_index, 0]
+        E = numpy.einsum('ij,ji', jcross, dm_elec)
+        logger.debug(self, 'Energy of e-n Comlomb interactions: %s', E)
+        return E
+
+    def nuc_nuc_coulomb(self, dm_nuc):
+        'the energy of Coulomb interactions between quantum nuclei'
+        mol = self.mol
+        E = 0
+        for i in range(len(dm_nuc)):
+            for j in range(len(dm_nuc)):
+                if j != i:
+                    jcross = scf.jk.get_jk((mol.nuc[i], mol.nuc[i], mol.nuc[j], mol.nuc[j]), dm_nuc[j], scripts='ijkl,lk->ij', aosym='s4') * mol._atm[mol.nuc[i].atom_index, 0] * mol._atm[mol.nuc[j].atom_index, 0]
+                    E += numpy.einsum('ij,ji', jcross, dm_nuc[i])
+
+        logger.debug(self, 'Energy of n-n Comlomb interactions: %s', E*.5) # double counted
+        return E*.5 
+
 
     def energy_tot(self, mf_elec, mf_nuc):
         'Total energy of NEO'
         mol = self.mol
         
         dm_elec = scf.hf.make_rdm1(mf_elec.mo_coeff, mf_elec.mo_occ)
-        dm_nuc = scf.hf.make_rdm1(mf_nuc.mo_coeff, mf_nuc.mo_occ)
+        dm_nuc = [None]*self.mol.nuc_num
+        for i in range(len(mf_nuc)):
+            dm_nuc[i] = scf.hf.make_rdm1(mf_nuc[i].mo_coeff, mf_nuc[i].mo_occ)
 
-        jcross = scf.jk.get_jk((mol.elec, mol.elec, mol.nuc, mol.nuc), dm_nuc, scripts='ijkl,lk->ij', aosym = 's4')
-        E_cross = numpy.einsum('ij,ij', jcross, dm_elec)
-
-        E_tot = mf_elec.e_tot + mf_nuc.e_tot - mf_nuc.energy_nuc() + E_cross
-
+        E_tot = 0
+        for i in range(len(mf_nuc)):
+            E_tot += mf_nuc[i].e_tot
+        logger.debug(self, 'Energy of quantum nuclei: %s', E_tot)
         logger.debug(self, 'Energy of electrons: %s',  mf_elec.e_tot)
-        logger.debug(self, 'Energy of quantum nuclei: %s', mf_nuc.e_tot)
-        logger.debug(self, 'Energy of classcial nuclei: %s', mf_nuc.energy_nuc())
-        logger.debug(self, 'Energy of e-p Comlomb interaction: %s', E_cross)
+        logger.debug(self, 'Energy of classcial nuclei: %s', mf_elec.energy_nuc())
+
+        E_tot += mf_elec.e_tot - self.elec_nuc_coulomb(dm_elec, dm_nuc) - self.nuc_nuc_coulomb(dm_nuc) - self.mol.nuc_num * mf_elec.energy_nuc() # substract repeatedly counted terms
 
         return E_tot
 
     def scf(self, conv_tol = 1e-7, max_cycle = 60, dm0_elec = None, dm0_nuc = None):
         'self-consistent field driver for NEO'
-        mol = self.mol
 
-        self.mf_elec.kernel()
+        # set up the Hamiltonian for electrons
+        self.mf_elec = scf.RHF(self.mol.elec)
+        self.mf_elec.init_guess = 'atom'
+        self.mf_elec.get_hcore = self.get_hcore_elec
+        self.dm_elec = self.mf_elec.get_init_guess(key='atom')
+
+        # set up the Hamiltonian for each quantum nucleus
+        self.mf_nuc = [None] * self.mol.nuc_num
+        for i in range(len(self.mol.nuc)):
+            self.mf_nuc[i] = scf.RHF(self.mol.nuc[i])
+            self.mf_nuc[i].get_init_guess = self.get_init_guess_nuc
+            self.mf_nuc[i].get_hcore = self.get_hcore_nuc
+            self.mf_nuc[i].get_veff = self.get_veff_nuc_bare
+            self.mf_nuc[i].get_occ = self.get_occ_nuc
+            self.dm_nuc[i] = self.get_init_guess_nuc(self.mol.nuc[i])
+
+        self.mf_elec.kernel(dump_chk=False)
         self.dm_elec = scf.hf.make_rdm1(self.mf_elec.mo_coeff, self.mf_elec.mo_occ)
 
-        self.mf_nuc.kernel()
+        for i in range(len(self.mf_nuc)):
+            self.mf_nuc[i].kernel(dump_chk=False)
+            self.dm_nuc[i] = scf.hf.make_rdm1(self.mf_nuc[i].mo_coeff, self.mf_nuc[i].mo_occ)
 
         E_tot = self.energy_tot(self.mf_elec, self.mf_nuc)
+        logger.info(self, 'Initial total Energy of NEO: %.15g' %(E_tot))
 
         scf_conv = False
         cycle = 0
@@ -166,13 +189,18 @@ class HF(SCF):
                 raise RuntimeError('SCF is not convergent within %i cycles' %(max_cycle))
 
             E_last = E_tot
-            self.dm_nuc = scf.hf.make_rdm1(self.mf_nuc.mo_coeff, self.mf_nuc.mo_occ)
-            self.mf_elec.kernel()
+            #self.dm_nuc = scf.hf.make_rdm1(self.mf_nuc.mo_coeff, self.mf_nuc.mo_occ)
+            self.mf_elec.kernel(dump_chk=False)
             self.dm_elec = scf.hf.make_rdm1(self.mf_elec.mo_coeff, self.mf_elec.mo_occ)
-            self.mf_nuc.kernel()
+            for i in range(len(self.mf_nuc)):
+                self.mf_nuc[i].kernel(dump_chk=False)
+                self.dm_nuc[i] = scf.hf.make_rdm1(self.mf_nuc[i].mo_coeff, self.mf_nuc[i].mo_occ)
+
             E_tot = self.energy_tot(self.mf_elec, self.mf_nuc)
             logger.info(self, 'Cycle %i Total Energy of NEO: %s' %(cycle, E_tot))
             if abs(E_tot - E_last) < conv_tol:
+                print(self.mf_elec.mo_energy)
+                print(self.mf_nuc[0].mo_energy)
                 scf_conv = True
                 logger.note(self, 'converged NEO energy = %.15g', E_tot) 
                 return E_tot
--- pyscf/neo/mole.py
+++ pyscf/neo/mole.py
@@ -6,23 +6,77 @@ from pyscf import gto
 from pyscf.lib import logger
 
 class Mole(gto.mole.Mole):
-    'a subclass of gto.mole.Mole to handle quantum nuclei in NEO'
+    '''A subclass of gto.mole.Mole to handle quantum nuclei in NEO.
+    By default, all atoms would be treated quantum mechanically.
 
-    def set_quantum_nuclei(self, alist):
+    Example:
+
+    >>> from pyscf import neo
+    >>> mol = neo.Mole()
+    >>> mol.build(atom = 'H 0 0 0; C 0 0 1.1; N 0 0 2.2', basis = 'ccpvdz')
+    >>> mol.set_quantum_nuclei() # all atoms are treated quantum mechanically
+    >>> mol.set_quantum_nuclei([0, 1]) # H and C are treated quantum mechanically
+
+    '''
+
+    def __init__(self, **kwargs):
+        gto.mole.Mole.__init__(self, **kwargs)
+
+    def elec_mole(self):
+        'return a Mole object for NEO-electron and classical nuclei'
+
+        eole = gto.mole.copy(self) # a Mole object for electrons
+        quantum_nuclear_charge = 0
+        for i in range(self.natm):
+            if self.quantum_nuc[i] == True:
+                quantum_nuclear_charge -= eole._atm[i,0]
+                eole._atm[i,0] = 0 # set the nuclear charge of quantum nuclei to be 0
+        eole.charge += quantum_nuclear_charge
+        return eole
+
+    def nuc_mole(self, atom_index, basis='etbs'):
+        'return a Mole object for specified quantum nuclei, the default basis is even-tempered Gaussian basis'
+        nole = gto.mole.copy(self) # a Mole object for quantum nuclei
+        nole.atom_index = atom_index
+
+        alpha = 2*math.sqrt(2)
+        beta = math.sqrt(2)
+
+        if basis == 'etbs':
+            basis = gto.expand_etbs([(0, 8, alpha, beta), (1, 8, alpha, beta), (2, 8, alpha, beta)]) # even-tempered basis 8s8p8d
+        nole._basis = gto.mole.format_basis({self.atom_symbol(atom_index): basis})
+        nole._atm, nole._bas, nole._env = gto.mole.make_env(nole._atom, nole._basis, self._env[:gto.PTR_ENV_START])
+        for i in range(self.elec.natm):
+            if self.quantum_nuc[i] == True:
+                nole._atm[i,0] = 0 # set the nuclear charge of quantum nuclei to be 0
+
+        #self.nuc.charge += int(self.nuc_num)
+        #self.nuc.nelectron = self.nuc_num
+        #self.nuc.spin = self.nuc_num
+        return nole
+
+    def build(self, alist = 'all', **kwargs):
         'assign which nuclei are treated quantum mechanically by a list(alist)'
-        self.quantum_nuc = [False]*self.natm
-        if alist is not None:
-            self.nuc_num = len(alist)
-            for i in alist:
-                if self.atom_pure_symbol(i) == 'H':
-                    self.quantum_nuc[i] = True
-                else:
-                    raise NotImplementedError('Only support quantum H now')
+        gto.mole.Mole.build(self, **kwargs)
 
-        logger.note(self, 'The H atom is set to be treated quantum-mechanically')
-        self.nuclei_expect_position = self.atom_coord(0) #beta
-        self.mole_elec()
-        self.mole_nuc()
+        self.quantum_nuc = [False]*self.natm
+
+        if alist == 'all':
+            self.quantum_nuc = [True]*self.natm
+            logger.note(self, 'All atoms are treated quantum-mechanically by default.')
+        elif isinstance(alist, list):
+            for i in alist:
+                self.quantum_nuc[i] = True
+                logger.note(self, 'The %s(%i) atom is treated quantum-mechanically' %(self.atom_symbol(i), i))
+        else:
+            raise NotImplementedError('Unsupported parameter %s' %(alist))
+
+        self.nuc_num = len([i for i in self.quantum_nuc if i == True]) # the number of quantum nuclei
+        self.elec = self.elec_mole()
+        self.nuc = []
+        for i in range(len(self.quantum_nuc)):
+            if self.quantum_nuc[i] == True:
+                self.nuc.append(self.nuc_mole(i))
 
     def set_nuclei_expect_position(self, position=None, unit='B'):
         'set an expectation value of the position operator for quantum nuclei(only support single proton now)'        
@@ -32,34 +86,5 @@ class Mole(gto.mole.Mole):
             self.nuclei_expect_position = position*1.88973
         elif unit == 'B':
             self.nuclei_expect_position = position
-
-    def mole_elec(self):
-        'return a Mole object for NEO-electron'
-
-        self.elec = gto.mole.copy(self)
-        for i in range(self.elec.natm):
-            if self.elec.quantum_nuc[i] == True:
-                self.elec._atm[i,0] = 0 # set the nuclear charge of quantum nuclei to be 0
-
-        self.elec.charge -= int(self.nuc_num) 
-        return self.elec
-
-    def mole_nuc(self):
-        'return a Mole object for quantum nuclei'
-        self.nuc = gto.mole.copy(self)
-
-        alpha = 2*math.sqrt(2)
-        beta = math.sqrt(2)
-        self.nuc_basis = gto.expand_etbs([(0, 8, alpha, beta), (1, 8, alpha, beta), (2, 8, alpha, beta)]) # even-tempered basis 8s8p8d
-        self.nuc._basis = gto.mole.format_basis({'H': self.nuc_basis}) #only support quantum H now
-        self.nuc._atm, self.nuc._bas, self.nuc._env = gto.mole.make_env(self.nuc._atom,self.nuc._basis, self._env[:gto.PTR_ENV_START])
-        for i in range(self.natm):
-            if self.quantum_nuc[i] == True:
-                self.nuc._atm[i,0] = 0 # set the nuclear charge of quantum nuclei to be 0
-
-        #self.nuc.charge += int(self.nuc_num)
-        #self.nuc.nelectron = self.nuc_num
-        #self.nuc.spin = self.nuc_num
-        return self.nuc
 
 
