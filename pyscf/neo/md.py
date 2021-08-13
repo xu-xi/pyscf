6e2851d18cceafd8210e241d8202ef9b72815766
xuxi0910796 <xuxi0910796@gmail.com>
2021-08-13 15:38:09 -0500

    data mirroring

--- pyscf/neo/md.py
+++ pyscf/neo/md.py
@@ -15,15 +15,16 @@ def step_average(data):
 
 def calc_ACF(traj):
     'calculate auto-correlation functions'
-    autocorr = signal.fftconvolve(traj, traj[::-1], mode='full')[len(traj)-1:] / len(traj)
+    autocorr = signal.correlate(traj, traj, mode='full')[len(traj)-1:]
+    autocorr = numpy.multiply(autocorr, 1 / numpy.arange(len(traj), 0, -1))
     return autocorr
 
-def hann2(length):
+def hann(length):
     'Hann window function'
     n = numpy.arange(length)
     return numpy.power(numpy.cos(math.pi*n/(2*(length-1))), 2)
 
-def vacf(datafile, start=1, end=-1, step=1):
+def vacf(datafile, start=0, end=-1, step=1):
     'calculate verlocity auto-correlation function (VACF) from trajectory of MD simulations'
     traj = Trajectory(datafile)
 
@@ -32,20 +33,23 @@ def vacf(datafile, start=1, end=-1, step=1):
         v.append(traj[i].get_velocities())
     v = numpy.array(v)
 
+    global n # the number of atoms
     t, n, x = v.shape
 
     mass = traj[-1].get_masses()
     acf = 0
 
-    for i in range(n):
-        for j in range(x):
+    for i in range(n): # i-th atom
+        for j in range(x): # j-th component of verlocity
             acf += calc_ACF(v[start:end:step,i,j]) * mass[i]
 
     return acf
 
-def dacf(datafile, time_step=0.5, start=1, end=-1, step=1):
+def dacf(datafile, time_step=0.5, start=0, end=-1, step=1):
     'calculate dipole auto-correlation function (DACF) from trajectory of MD simulations'
     traj = Trajectory(datafile)
+    global n # the number of atoms
+    n = len(traj[-1].get_positions())
 
     dipole = []
     for i in range(len(traj)):
@@ -65,29 +69,36 @@ def dacf(datafile, time_step=0.5, start=1, end=-1, step=1):
 def calc_FFT(acf):
     'get Fourier transform of ACF'
 
+    # window function
+    acf *= hann(len(acf))
+
     # zero padding
-    N = int(2 ** math.ceil(math.log(len(acf), 2)))
+    N = 2 ** (math.ceil(math.log(len(acf), 2)) + 2)
+    acf = numpy.append(acf, numpy.zeros(N - len(acf)))
 
     # data mirroring
-    #acf = numpy.concatenate((acf, acf[1::-1]), axis=0)
+    acf = numpy.concatenate((acf, acf[::-1][:-1]), axis=0)
 
-    yfft = numpy.fft.fft(acf, N, axis=0)
-    # return numpy.square(numpy.absolute(yfft))
+    yfft = numpy.fft.fft(acf, axis=0)
+
+    #numpy.set_printoptions(threshold=numpy.inf)
+    #print(yfft)
+
     return yfft
 
 def spectrum(acf, time_step=0.5, corr_depth=4096):
     'get wavenumber and intensity of spectrum'
 
     acf = acf[:corr_depth]
-    acf *= hann2(len(acf))
+
     yfft = calc_FFT(acf)
 
     fs2cm = 1e-15 * units._c * 100
 
     wavenumber = numpy.fft.fftfreq(len(yfft), time_step * fs2cm)[0:int(len(yfft)/2)]
-    intensity = yfft[0:int(len(yfft)/2)]
-    factor = 11604.52500617 * units.fs * fs2cm # eV2K * au2cm -> K*cm
+    intensity = numpy.real(yfft[0:int(len(yfft)/2)])
+    factor = 11604.52500617 * fs2cm / (3 * n) # eV2K * fs2cm -> K*cm
     intensity *= factor
     temperature = scipy.integrate.cumtrapz(intensity, wavenumber)
-    return wavenumber, intensity, temperature 
-
+    print('Integrated temperature (K)', temperature[-1])
+    return wavenumber, intensity, temperature
